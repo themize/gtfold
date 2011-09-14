54cfe37819d01b6480440d6aa86e5222a33e3dee
Prashant Gaurav <pgaurav3@gatech.edu>
2011-09-14 11:04:58 -0400

    adding partitition function header/src files

---
+++ gtfold-mfe/src/partition-func.c
@@ -0,0 +1,36 @@
+#include "partition-func.h"
+
+static void create_partition_arrays();
+static void init_partition_arrays();
+static void fill_partition_arrays();
+static void free_partition_arrays();
+
+static void calc_u(int i, int j);
+static void calc_ud(int i, int j);
+static void calc_up(int i, int j);
+static void calc_upm(int i, int j);
+static void calc_u1(int i, int j);
+static void calc_u1d(int i, int j);
+static void calc_s1(int i, int j);
+static void calc_s2(int i, int j);
+static void calc_s3(int i, int j);
+
+void create_partition_arrays()
+{
+
+}
+
+void init_partition_arrays()
+{
+		
+}
+
+void fill_partition_arrays()
+{
+
+}
+
+void free_partition_arrays()
+{
+
+}
