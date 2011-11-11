cdd5c97417e064260bd818a17ce1ff9cab35e260
Rohit Banga <iamrohitbanga@ubuntu.(none)>
2011-11-11 10:33:56 -0800

    Pretty Output for Suboptimal Structures Match Test. Output sent to a separate file.

--- gtfold-mfe/tests/scripts/SuboptMatchStructures.pm
+++ gtfold-mfe/tests/scripts/SuboptMatchStructures.pm
@@ -1,5 +1,5 @@
 #!/usr/bin/perl
-package SuboptNumStructures;
+package SuboptMatchStructures;
 use strict;
 use warnings;
 use File::Basename;
@@ -14,12 +14,24 @@ sub test()
   my $gtdir = $Config{"G_GTFOLD_DIR"};
   my $rnadir = $Config{"G_RNAFOLD_DIR"};
   my $workdir = $Config{"G_WORK_DIR"};
+  my $min_energy = $Config{"L_SUBOPTMATCHSTRUCTURES_MIN_ENERGY"};
+  my $max_energy = $Config{"L_SUBOPTMATCHSTRUCTURES_MAX_ENERGY"};
+  my $energy_stride = $Config{"L_SUBOPTMATCHSTRUCTURES_ENERGY_STRIDE"};
+
+  if (not(defined($min_energy))) {
+    $min_energy = 0;
+  }
+  if (not(defined($max_energy))) {
+    $max_energy = 3;
+  }
+  if (not(defined($energy_stride))) {
+    $energy_stride = 1;
+  }
 
   my $key;
   my $value;
   my %new_hash = (%local_sequences);
-  # For testing pseudoknot detection logic
-  # we may only test the sequences specified in local_sequences
+
   while (($key, $value) = each(%new_hash)) {
 
 	  my $seqname=$key;
@@ -32,8 +44,11 @@ sub test()
     my $rnaout = "$workdir$seqname-rna";
 	  my $gtoutfilename  = $workdir."$seqname-gt.ct";
 
+    my %gtstruct_hash;
+    my %rnastruct_hash;
+
     my $energy;
-    for ($energy = 1; $energy <=10; ++$energy) {
+    for ($energy=$min_energy; $energy<=$max_energy; $energy=$energy+$energy_stride) {
 
       my $gtfile = $gtout.$energy;
       my $rnafile = $rnaout.$energy."_ss.txt";
@@ -50,19 +65,60 @@ sub test()
 
       my $gtsorted = $gtsubopt_file."_sorted";
       my $rnasubopt_sorted = $rnafile."_sorted";
-      `cat $gtsubopt_file | sed '/[A-Z]/d' | sort -o $gtsorted`;
-      `cat $rnasubopt_file | sed '/[A-Z]/d' | sort -o $rnasubopt_sorted`;
-      #`sort $gtsubopt_file -o $gtsorted`;
-      #`sort $rnasubopt_file -o $rnasubopt_sorted`;
+      my $gtstructs = `cat $gtsubopt_file | sed '/[A-Z]/d' | sed '/^\$/d'`;
+      my $rnastructs = `cat $rnasubopt_file | sed '/[A-Z]/d' | sed '/^\$/d'`;
 
-      my $diff_str = `diff $gtsorted $rnasubopt_sorted`;
-      #print $diff_str;
+      %gtstruct_hash = split('[ \n]',$gtstructs);
+      %rnastruct_hash = split('[ \n]',$rnastructs);
 
-      if ($diff_str eq "") {
+      my $diff_file_name = "$workdir$seqname\_$energy.diff"; 
+
+      open (DIFF_FILE, ">> $diff_file_name");
+
+      my $diff = 0;
+      foreach my $key ( keys %gtstruct_hash )
+      {
+        my $change = $gtstruct_hash{$key} 
+        unless ( (exists $rnastruct_hash{$key}) && $gtstruct_hash{$key} eq $rnastruct_hash{$key} );
+        # gotta check for existence to quiet warnings.
+        if(defined($change) && $change ne "") {
+          print DIFF_FILE $key. "\nGTFOLD Energy: ". $gtstruct_hash{$key};
+          if (exists $rnastruct_hash{$key}) {
+            print DIFF_FILE "\nRNASubopt Energy: ". $rnastruct_hash{$key};
+          }
+          else {
+            print DIFF_FILE "\nRNASubopt Energy: No corresponding structure";
+          }
+          print DIFF_FILE "\n";
+          $diff = 1;
+        }
+      }
+
+      foreach my $key ( keys %rnastruct_hash )
+      {
+        my $change = $rnastruct_hash{$key} 
+        unless ( (exists $gtstruct_hash{$key}) && $gtstruct_hash{$key} eq $rnastruct_hash{$key} );
+        # gotta check for existence to quiet warnings.
+        if(defined($change) && $change ne "") {
+          print DIFF_FILE $key. "\nRNASubopt Energy: ". $rnastruct_hash{$key};
+          if (exists $gtstruct_hash{$key}) {
+            print DIFF_FILE "\nGTFOLD Energy: ". $gtstruct_hash{$key};
+          }
+          else {
+            print DIFF_FILE "\nGTFOLD Energy: No corresponding structure";
+          }
+          print DIFF_FILE "\n";
+          $diff = 1;
+        }
+      }
+
+      close DIFF_FILE;
+
+      if ($diff == 0) {
         $logger->info("TEST PASSED: $seqname: energy delta = $energy: Suboptimal Structures matched");
       }
       else {
-        $logger->error("TEST FAILED: $seqname: energy delta = $energy: Suboptimal Structures not matched\n $diff_str");
+        $logger->error("TEST FAILED: $seqname: energy delta = $energy: Suboptimal Structures not matched. Difference Written to file $diff_file_name");
       }
     }
   }
